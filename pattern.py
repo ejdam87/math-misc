1d28f569abbae738abcb0b432fc14087192ef6bd
ejdam87 <adam.dzadon@gmail.com>
2022-12-18 12:33:22 +0100

    Refactor pattern repr

--- pattern.py
+++ pattern.py
@@ -1,8 +1,9 @@
 from PIL import Image, ImageDraw
 from typing import List, Tuple
+from math import sqrt
 
-Pattern  =  List[ List[ bool ] ]
-Position = Tuple[ int, int ]
+Pattern  =  List[ int ]
+EMPTY = -1
 
 WIDTH  = 600
 HEIGHT = 600
@@ -20,87 +21,34 @@ def draw(pattern: Pattern) -> None:
     im = Image.new( "RGB", (WIDTH, HEIGHT), BG )
     dr = ImageDraw.Draw( im )
 
-    m = len( pattern )
-    n = len( pattern[0] )
+    points = [None for _ in range( len(pattern) )]
+    n = int( sqrt( len(pattern) ) )
 
     step_x = WIDTH // n
-    step_y = HEIGHT // m
+    step_y = HEIGHT // n
 
     rx = step_x // 12
     ry = step_y // 12
 
-    prev = None
-
-    for y in range( m ):
+    for y in range( n ):
         cy = y * step_y + step_y // 2
         for x in range( n ):
             cx = x * step_x + step_x // 2
 
-            color = SELECED_DOT if pattern[ y ][ x ] else BASE_DOT
+            color = SELECED_DOT if pattern[ y * n + x ] != EMPTY else BASE_DOT
             dr.ellipse( [ (cx - rx, cy - ry), (cx + rx, cy + ry) ], fill=color, outline=color )
 
-            if not pattern[ y ][ x ]:
+            if pattern[ y * n + x ] == EMPTY:
                 continue
 
-            curr = (cx, cy)
-            if prev is not None:
-                dr.line( [ prev, curr ], fill=LINE, width=LINE_WIDTH )
-            prev = curr
+            points[ pattern[ y * n + x ] ] = (cx, cy)
+
+    for fst, snd in zip( points, points[1:] ):
+        if fst is None or snd is None:
+            continue
+        dr.line( [ fst, snd ], fill=LINE, width=LINE_WIDTH )
 
     im.show()
 
-
-## We transform the task from finding all possible pattern locks
-## to find all subtrees from graph given by pattern vertices
-## ( we assume it's the same task )
-
-def subpatterns(n: int) -> List[ Pattern ]:
-    res = []
-    pattern = [ [ False for _ in range(3) ] for _ in range(3) ]
-    subpatterns_rec(pattern, n, res)
-    return res
-
-## surounding + positions where abs(x1 - x2) == 1 && abs(y1 - y2) > 1
-## ( same vice-versa ) or empty pattern
-def possible(p: Pattern, x: int, y: int) -> bool:
-    
-    empty = True
-    for py in range( len(p) ):
-        for px in range( len(p[0]) ):
-            if p[ py ][ px ]:
-
-                empty = False
-
-                ## Surrounding
-                if abs(px - x) <= 1 and abs(py - y) <= 1:
-                    return True
-
-                if abs(px - x) == 1 and abs(py - y) > 1:
-                    return True
-
-                if abs(py - y) == 1 and abs(px - x) > 1:
-                    return True
-
-    return empty
-
-def subpatterns_rec(curr: Pattern,
-                    n: int,
-                    res: List[ Pattern ]) -> None:
-    
-    if n == 0:
-        if curr not in res:
-            res.append(  [ [ curr[y][x] for x in range(len(curr[0])) ] for y in range(len(curr)) ] )
-        return
-
-    for y in range( len(curr) ):
-        for x in range( len(curr[0]) ):
-            if not curr[ y ][ x ] and possible(curr, x, y):
-                curr[ y ][ x ] = True
-                subpatterns_rec(curr, n - 1, res)
-                curr[ y ][ x ] = False
-
-
-patterns = subpatterns(9)
-print( len(patterns) )
-for p in patterns:
-    draw(p)
+p = [-1, -1, -1,   0, 1, -1,   -1, -1, -1]
+draw(p)
