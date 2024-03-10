dee1629cfd6157e584f10b7dbe6cc4355622bf27
ejdam87 <adam.dzadon@gmail.com>
2024-03-10 12:57:28 +0100

    AISA

--- estimates.py
+++ estimates.py
@@ -83,4 +83,53 @@ def conf_interval_var(samples: list[float],
 
     q1 = qchisq(1 - confidence / d, df=n-1)
     q2 = qchisq(confidence / d, df=n-1)
+
+    ## RETURNS VARIANCE VALUES (NOT DEVIATION)
     return ( (n - 1) * var / q1, (n - 1) * var / q2 )
+
+
+def conf_interval_meandiff(samples1: list[float],
+                           samples2: list[float],
+                           confidence: float,
+                           sigma1: float|None=None,
+                           sigma2: float|None=None) -> tuple[float, float]:
+    
+    sigma_known = False if (sigma1 is None) or (sigma2 is None) else True
+
+    m1 = mean(samples1)
+    m2 = mean(samples2)
+    n1 = len(samples1)
+    n2 = len(samples2)
+
+    if sigma_known:
+        q = qnorm(1 - confidence / 2)
+        diff = q * np.sqrt( ((sigma1**2)/n1) + ((sigma2**2)/n2) )
+        return ( (m1 - m2) - diff, (m1 - m2) + diff )
+    ## HERE WE ASSUME sigma1 == sigma2 (despite they are unknown)
+    else:
+        q = qt(1 - confidence / 2, df=n1+n2-2)
+        var1 = sample_var(samples1)
+        var2 = sample_var(samples2)
+        var12 = ((n1 - 1) * var1 + (n2 - 1) * var2) / (n1 + n2 - 2)
+        sd12 = np.sqrt(var12)
+        n12 = np.sqrt( (n1 + n2) / (n1 * n2) )
+
+        diff = q * sd12 * n12
+        return ( (m1 - m2) - diff, (m1 - m2) + diff )
+
+
+def conf_interval_sigmarat(samples1: list[float],
+                           samples2: list[float],
+                           confidence: float) -> tuple[float, float]:
+    
+    n1 = len(samples1)
+    n2 = len(samples2)
+    var1 = sample_var(samples1)
+    var2 = sample_var(samples2)
+
+    rat = var1 / var2
+    q1 = qf(1 - confidence / 2, df1=n1-1, df2=n2-1)
+    q2 = qf(confidence / 2, df1=n1-1, df2=n2-1)
+
+    ## RETURNS RATIO OF VARIANCES !!!
+    return (rat * (1 / q1), rat * (1 / q2))
